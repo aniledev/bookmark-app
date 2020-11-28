ff95676e615583cc635a3591649ffdcb660064d8
aniledev <elinamcgill@gmail.com>
2020-11-28 05:09:18 -0500

    Add bookmark html generation template function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,48 @@
 import $ from "jquery";
 
+//  STORE DATA SINGLE SOURCE OF TRUTH
+
 // TEMPLATE GENERATION FUNCTIONS
 
+const generateAddForm = function () {
+  return `<div id="form" class="form">
+  <h3>Add a new bookmark</h3>
+  <form action="">
+    <label for="url">URL</label>
+    <input
+      type="text"
+      id="url"
+      name="url"
+      placeholder="www.samplesite.com"
+    />
+    <label for="rating">Rating</label>
+    <input type="number" id="rating" name="rating" placeholder="3" />
+    <label for="description">Description</label>
+    <textarea
+      name="description"
+      id="description"
+      cols="30"
+      rows="10"
+      placeholder="Enter a description (optional)"
+    ></textarea>
+  </form>
+</div>
+<div class="button">
+  <button id="cancel" class="cancel">Cancel</button>
+  <button id="create" class="create">Create</button>
+</div>`;
+};
+
 // EVENT HANDLER FUNCTIONS
+
 const handleNewItemClick = function (event) {
   $(".top-button").on("click", ".new", (event) => {
     // code that you want to execute
     console.log("new item button clicked");
-    // render();
+    let html = "";
+    html = generateAddForm();
+    $(".top-button").empty();
+    $("").html(html);
   });
 };
 
