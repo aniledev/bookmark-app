b4591182977c253dc7e6862c9c6bb6a6302b326b
aniledev <elinamcgill@gmail.com>
2020-11-28 05:31:06 -0500

    Updated store. Added pseudocode for event handlers

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,15 @@
 import $ from "jquery";
 
-//  STORE DATA SINGLE SOURCE OF TRUTH
+//  EXAMPLE STORE DATA SINGLE SOURCE OF TRUTH
+let store = {
+  bookmarks: [
+    { id: 1, title: javascript, rating: 3, expanded: false, filtered: false },
+    { id: 2, title: css, rating: 4, expanded: false, filtered: false },
+    { id: 3, title: programming, rating: 2, expanded: false, filtered: false },
+  ],
+  adding: false,
+  filtering: false,
+};
 
 // TEMPLATE GENERATION FUNCTIONS
 
@@ -38,6 +47,10 @@ const generateAddForm = function () {
 const handleNewItemClick = function (event) {
   $(".top-button").on("click", ".new", (event) => {
     // code that you want to execute
+    // console log to indicated user clicked button
+    // change adding state to true in store
+    // if adding is true generate add form html in the render function
+    // render add form html
     console.log("new item button clicked");
     let html = "";
     html = generateAddForm();
@@ -49,14 +62,30 @@ const handleNewItemClick = function (event) {
 const handleFilterClick = function () {
   $(".top-button").on("click", ".filter", (event) => {
     // code that you want to execute
+    // console log to indicate user clicked the button
+    // change filtering state in store to true
+    // if filtered state is true generate the dropdown html in the render function
     console.log("filter button clicked");
     render();
+  });
+};
+
+const handleFilterDropdown = function () {
+  $("").on("", "", (event) => {
+    //code that you want to excute
+    // when user selects a dropbox rating option
+    // hide all bookmarks that are less than this rating / show bookmarks that are equal or greater than this rating using the store data
+    // change filtered: false to true for store items
+    //render the html
+    console.log("user selected filter option");
   });
 };
 
 const handleDeleteItemClick = function () {
   $(".bookmark").on("click", ".delete", (event) => {
     // code that you want to execute
+    //console log that user clicked button
+    // when user clicks delete button, use splice to remove specific bookmark from store
     console.log("delete bookmark button clicked");
     render();
   });
@@ -65,6 +94,9 @@ const handleDeleteItemClick = function () {
 const handleClearFilterClick = function () {
   $(".button").on("click", ".clear-filter", (event) => {
     // code that you want to execute
+    //console log that user clicked button
+    // change filtering state in store to false
+    // if filtering state is false generate html without dropdown box/home state of app
     console.log("clear filter button clicked");
     render();
   });
@@ -73,6 +105,9 @@ const handleClearFilterClick = function () {
 const handleExpandItemClick = function () {
   $(".bookmark").on("click", ".title", (event) => {
     // code that you want to execute
+    // console log that user clicked button
+    // for that item change expanded in store to true
+    // render new html for expanded bookmark
     console.log("expand bookmark clicked");
     render();
   });
